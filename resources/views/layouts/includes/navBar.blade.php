54bf806fd32c7b2ef7e5af88912ca76a97320238
simon <simongouzien@hotmail.fr>
2023-01-12 18:26:48 +0100

    add project struture, table, template, and controller

---
+++ resources/views/layouts/includes/navBar.blade.php
@@ -0,0 +1,20 @@
+<a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-outline rounded-pill"><i class="fa fa-list"></i></a>
+<a href="/users" class="btn btn-outline rounded-pill"><i class="fa fa-user"></i> User</a>
+<a href="{{ route('orders.index')}}" class="btn btn-outline rounded-pill"><i class="fa fa-box"></i> Products</a>
+<a href="{{ route('products.index')}}" class="btn btn-outline rounded-pill"><i class="fa fa-laptop"></i> Cashier</a>
+<a href="{{ route('transactions.index')}}" class="btn btn-outline rounded-pill"><i class="fa fa-file"></i> Report</a>
+<a href="{{ route('orders.index')}}" class="btn btn-outline rounded-pill"><i class="fa fa-money-bill"></i> Echanges</a>
+<a href="{{ route('orders.index')}}" class="btn btn-outline rounded-pill"><i class="fa fa-chart"></i> Suppliers</a>
+<a href="{{ route('orders.index')}}" class="btn btn-outline rounded-pill"><i class="fa fa-users"></i> Customers</a>
+
+<style>
+    .btn-outline{
+        border-color: #008888;
+        color: #008888;
+    }
+    .btn-outline:hover{
+        background: #008888;
+        color: #fff;
+
+    }
+</style>
---
+++ resources/views/layouts/includes/sideBar.blade.php
@@ -0,0 +1,47 @@
+<nav class="active" id="sidebar">
+    <ul class="list-unstyled lead">
+        <li class="active">
+            <a href=""><i class="fa fa-home fa-lg"></i> Home</a>
+        </li>
+        <li>
+            <a href="{{ route('orders.index')}}"><i class="fa fa-box fa-lg"></i> Orders</a>
+        </li>
+        <li>
+            <a href="{{ route('transactions.index')}}"><i class="fa fa-regular fa-money-bill"></i> Transactions</a>
+        </li>
+        <li>
+            <a href="{{ route('products.index')}}"><i class="fa fa-duotone fa-truck"></i> Transactions</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    #sidebar ul.lead {
+        border-bottom: 1px solid #47748b;
+        width: fit-content;
+
+    }
+
+    #sidebar ul li a {
+        padding: 10px;
+        font-size: 1.1em;
+        display: block;
+        width: 30vh;
+        color: #008888;
+    }
+    #sidebar ul li a:hover {
+        color: #fff;
+        background: #008888;
+        text-decoration: none !important;
+
+    }
+    #sidebar ul li a i {
+        margin-right: 10px;
+    }
+
+    #sidebar ul li.active>a,
+    a[aria-expended="true"] {
+        color: #333;
+        background: #008888;
+    }
+</style>
